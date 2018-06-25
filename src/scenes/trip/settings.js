455529b5d39060834caad4c0ecc1c873568c438a
Ben Hadfield <benjohnhadfield@gmail.com>
2018-06-25 12:16:10 +0100

    Small style updates for sidebar and nav (#2)
    * add trip list and create functionality
    
    * load profile on app start, add trip get actions
    
    * custom button, add bootstrap js dependencies (revert in future if causes js bugs)
    
    * add logout, cleanup imports
    
    * trip title in nav
    
    * sidebar styles
    
    * add redirect logic after authentication
    
    * icon component, navbar dropdown refactor
    
    * adds join form component and scene
    
    * redux store refactor
    
    * basic place search with autocomplete
    
    * fix debounce on location search, move route authentication checks to routes.js file
    
    * namespace trips actions, add wtihTripDetails update logic to prevent redundant requests/action calls
    
    * rename module/trips -> module/trip
    
    * refactor sidebar
    
    * nav spacing, onCancel for location add

--- src/scenes/trip/settings.js
+++ src/scenes/trip/settings.js
@@ -1,19 +1,45 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import { withTripDetail } from '../../enhancers/load'
 import { NavSidebarFrame } from '../../modules/core/frame'
+import trip from '../../modules/trip'
 
 class Settings extends React.Component {
+    constructor (props) {
+        super(props)
+        this.actions = bindActionCreators(trip.actions.trips, props.dispatch)
+    }
+
+    onDelete = e => {
+        const { history, activeTrip } = this.props
+        this.actions.remove(activeTrip.id)
+            .then(this.actions.list)
+            .then(() => history.push('/'))
+    }
+
     render () {
         return (
             <NavSidebarFrame>
                 Settings {this.tripId}
+                <div className='card'>
+                    <div className='card-body'>
+                        <button
+                            className='btn btn-danger'
+                            onClick={this.onDelete}
+                        >
+                            Delete Trip
+                        </button>
+                    </div>
+                </div>
             </NavSidebarFrame>
         )
     }
 }
 
-const mapStateToProps = state => state
+const mapStateToProps = state => ({
+    activeTrip: trip.selectors.activeTripSelector(state)
+})
 
 export default connect(mapStateToProps)(withTripDetail(Settings))
